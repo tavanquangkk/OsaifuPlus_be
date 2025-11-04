73da33de43635aa13e302cec633c1cdf7bd2071c
taquang <taquanga6dp@gmail.com>
2025-11-04 22:41:59 +0900

    add croesFilter config

---
+++ src/main/kotlin/jp/tvq/osaifuplus/utils/CorsFilter.kt
@@ -0,0 +1,18 @@
+package jp.tvq.osaifuplus.utils
+
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.container.ContainerResponseContext
+import jakarta.ws.rs.container.ContainerResponseFilter
+import jakarta.ws.rs.ext.Provider
+
+@Provider
+class CorsFilter: ContainerResponseFilter {
+    override fun filter(requestContext: ContainerRequestContext?, responseContext: ContainerResponseContext?) {
+        responseContext?.headers?.add("Access-Control-Allow-Origin", "http://localhost:5173")
+        responseContext?.headers?.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+        responseContext?.headers?.add("Access-Control-Allow-Credentials", "true")
+        responseContext?.headers?.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+    }
+
+
+}
